8fc6eb85a4b7022fae501610200fb424c0f8a91d
NikiNurAstri <2057401012@buminusantara.ac.id>
2022-12-13 09:56:28 -1200

    Initial commit

---
+++ app/src/main/java/nikinurastri/my/tokoonline/MainActivity.kt
@@ -0,0 +1,11 @@
+package nikinurastri.my.tokoonline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
